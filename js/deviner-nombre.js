d79bc2e42beb1d93b534f61f02e3ae18d83605c4
Maxik_22 <94693049+MaximilienMoritz@users.noreply.github.com>
2022-08-30 16:04:32 +0200

    exercice fini

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,9 +1,9 @@
 /**
-* Jeu du nombre mystère
-* @author  Steve Fallet
-* @version 0.1
-* @since   2018-09-09 (date de création)
-*/
+ * Jeu du nombre mystère
+ * @author  Steve Fallet
+ * @version 0.1
+ * @since   2018-09-09 (date de création)
+ */
 
 // Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
 (function main() {
@@ -17,6 +17,35 @@
      */
     function tireNombre(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    let nbEssais = 0;
+    let test = false;
+    let nbMystere = tireNombre(1, 100);
+
+    //Tant que l'utilisateur n'a pas trouvé le nombre (test est faux)
+    //Répète la saisie
+    while (!test) {
+
+        //Génère un nombre aléatoire compris entre 1 et 100
+        let reponse = Number(prompt("Entrez un nombre entre 1 et 100"));
+
+        //Incrémente le nombre d'essais
+        nbEssais++;
+
+        //Si l'utilisateur trouve le nombre
+        //Affiche une confirmation et le nombre d'essais
+        if (reponse === nbMystere) {
+            alert(`Vous avez trouvé après ${nbEssais} essais`);
+            test = true;
+
+            //Si le nombre est plus grand / petit
+            //Affiche un message en accordance
+        } else if (reponse < nbMystere) {
+            alert("C'est plus grand");
+        } else {
+            alert("C'est plus petit");
+        }
     }
 
 }()); // main IIFE
